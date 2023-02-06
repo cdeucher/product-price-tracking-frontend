c69ff4d3f8654348aeb6a70f11e04ff90a6275ae
Cristiano Deucher <1205053+cdeucher@users.noreply.github.com>
2023-02-05 21:07:14 -0300

    add: register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppService } from "./app.service";
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
